d8a69003a8c7c162a8c07d27742924138af7335c
Jeffrey Jeyachandren <jeffreyjeyachandren@Jeffreys-Mac-mini.local>
2024-11-09 22:55:01 -0500

    update models.py with groq llama and mixtral

--- src/agents/models.py
+++ src/agents/models.py
@@ -10,14 +10,28 @@ from langchain_openai import ChatOpenAI
 # if the /stream endpoint is called with stream_tokens=True (the default)
 models: dict[str, BaseChatModel] = {}
 if os.getenv("OPENAI_API_KEY") is not None:
-    models["gpt-4o-mini"] = ChatOpenAI(model="gpt-4o-mini", temperature=0.5, streaming=True)
+    models["gpt-4o-mini"] = ChatOpenAI(
+        model="gpt-4o-mini", temperature=0.5, streaming=True
+    )
     models["gpt-4o"] = ChatOpenAI(model="gpt-4o", temperature=0.5, streaming=True)
     models["gpt-4"] = ChatOpenAI(model="gpt-4-turbo", temperature=0.5, streaming=True)
-    models["gpt-3.5"] = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5, streaming=True)
-    models["o1-preview"] = ChatOpenAI(model="o1-preview", temperature=0.5, streaming=True)
+    models["gpt-3.5"] = ChatOpenAI(
+        model="gpt-3.5-turbo", temperature=0.5, streaming=True
+    )
+    models["o1-preview"] = ChatOpenAI(
+        model="o1-preview", temperature=0.5, streaming=True
+    )
     models["o1-mini"] = ChatOpenAI(model="o1-mini", temperature=0.5, streaming=True)
 if os.getenv("GROQ_API_KEY") is not None:
-    models["llama-3.1-70b"] = ChatGroq(model="llama-3.1-70b-versatile", temperature=0.5)
+    models["llama-3.1-70b-versatile"] = ChatGroq(
+        model="llama-3.1-70b-versatile", temperature=0.5
+    )
+    models["llama-3.1-8b-instant"] = ChatGroq(
+        model="llama-3.1-8b-instant", temperature=0.5
+    )
+    models["mixtral-8x7b-32768"] = ChatGroq(
+        model="llama-3.1-405b-reasoning", temperature=0.5
+    )
 if os.getenv("GOOGLE_API_KEY") is not None:
     models["gemini-1.5-flash"] = ChatGoogleGenerativeAI(
         model="gemini-1.5-flash", temperature=0.5, streaming=True

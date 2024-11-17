7e711677900963707cebe88287f9ebc300153788
JeffinWithYa <jjeyachandren@gmail.com>
2024-11-17 16:28:56 -0500

    trained model results

--- fine_tuning/train_model.py
+++ fine_tuning/train_model.py
@@ -3,7 +3,7 @@ import time
 
 client = OpenAI()
 
-# Upload the fine-tuning file
+# Step 1: Upload the fine-tuning file
 """
 client.files.create(
     file=open(
@@ -13,9 +13,8 @@ client.files.create(
     purpose="fine-tune",
 )
 """
-
+# Step 2: List all files
 """
-# List all files
 files = client.files.list()
 
 # Print each file's id and name
@@ -23,14 +22,15 @@ for file in files:
     print(f"File ID: {file.id}, Filename: {file.filename}, Purpose: {file.purpose}")
 """
 
+# Step 3: Fine-tune the model
 """
-# Fine-tune the model
 client.fine_tuning.jobs.create(
-    training_file="file-yTOJ45lteVtUB1NAf8grqOL9", model="gpt-4o-mini-2024-07-18"
+    training_file="file-yTOJ45lteVtUB1NAf8grqOL9", model="gpt-3.5-turbo-0125"
 )
 """
+
+# Step 4: List all fine-tuning jobs
 """
-# List all fine-tuning jobs
 jobs = client.fine_tuning.jobs.list()
 
 # Print details of each job
@@ -42,10 +42,10 @@ for job in jobs:
     print("---")
 """
 
-# Continuously monitor the fine-tuning job status
+# Step 5: Continuously monitor the fine-tuning job status
 
 while True:
-    job_status = client.fine_tuning.jobs.retrieve("ftjob-VJowv6ZvPPUgX89LNpiAqTgf")
+    job_status = client.fine_tuning.jobs.retrieve("ftjob-aFriIe6c56K4GE0TCFozshXD")
     print(f"Status: {job_status.status}")
 
     if job_status.status in ["succeeded", "failed", "cancelled"]:
